23be2718ff645f0cba0006a182ad2b20e064f6f3
Lucas Cruz <lucascs20182@gmail.com>
2021-07-10 15:42:14 -0300

    inicie a implementação da tela inicial depois do usuário logado

---
+++ src/components/HomeLogada.js
@@ -0,0 +1,141 @@
+import React from 'react';
+
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView} from 'react-native';
+
+import { Ionicons, MaterialIcons } from '@expo/vector-icons';
+
+// import Home from './src/components/Home';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+        <View style={styles.row}>
+            <Text style={styles.boasVindas}>Olá, Mari</Text>
+
+            <View style={styles.containerIcones}>
+                <TouchableOpacity style={styles.elipseIcone}>
+                    <Ionicons name="eye-outline" size={25} color="white" />
+                </TouchableOpacity>
+                
+                <TouchableOpacity style={styles.elipseIcone}>
+                    <Ionicons name="settings-outline" size={27} color="white" />
+                </TouchableOpacity>
+            </View>            
+        </View>
+        <View style={styles.containerContas}>
+
+        </View>
+
+        <View style={styles.containerContas}>
+
+        </View>
+
+        <View style={styles.containerFooter}>
+            <ScrollView contentContainerStyle={styles.tabContainer} 
+                horizontal showsHorizontalScrollIndicator={false}>
+                    <View style={styles.tabItem}>
+                        <MaterialIcons name="person-add" size={24} color="#fff" />
+                        <Text style={styles.itemFooterText}>Pix</Text>
+                    </View>
+
+                    <View style={styles.tabItem}>
+                        <MaterialIcons name="person-add" size={24} color="#fff" />
+                        <Text style={styles.itemFooterText}>Pagar</Text>
+                    </View>
+
+                    <View style={styles.tabItem}>
+                        <MaterialIcons name="person-add" size={24} color="#fff" />
+                        <Text style={styles.itemFooterText}>Indicar amigos</Text>
+                    </View>
+
+                    <View style={styles.tabItem}>
+                        <MaterialIcons name="person-add" size={24} color="#fff" />
+                        <Text style={styles.itemFooterText}>Transferir</Text>
+                    </View>
+
+                    <View style={styles.tabItem}>
+                        <MaterialIcons name="person-add" size={24} color="#fff" />
+                        <Text style={styles.itemFooterText}>Depositar</Text>
+                    </View>
+            </ScrollView>
+        </View>
+
+        
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-around',
+        backgroundColor: '#830AD1',
+        paddingTop: 60
+    },
+
+    containerIcones: {
+        flexDirection: 'row',        
+    },
+    
+    elipseIcone: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        width: 48,
+        height: 48,
+        backgroundColor: '#8E31D0',
+        borderRadius: 100,
+        marginHorizontal: 5
+    },
+
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        width: '90%'  
+    },
+
+    boasVindas: {
+        width: 150,
+        height: 27,
+
+        // fontFamily: 'Inter',
+        fontStyle: 'normal',
+        fontWeight: 'bold',
+        fontSize: 26,
+        lineHeight: 27,
+        letterSpacing: 2,
+        color: "#fff"
+    },
+
+    containerContas: {
+        width: 329,
+        height: 145,
+        backgroundColor: '#FFFFFF',
+        borderRadius: 4
+    },
+
+    containerFooter: {
+        flexDirection: 'row',
+    },
+
+    tabContainer: {
+        paddingLeft: 5,
+        paddingRight: 20
+    },
+
+    tabItem: {
+        width: 85,
+        height: 85,
+        backgroundColor: 'rgba(255, 255, 255, .2)',
+        borderRadius: 3,
+        marginLeft: 10,
+        padding: 10,
+        justifyContent: 'space-between'
+    },
+
+    itemFooterText: {
+        fontSize: 13,
+        color: '#fff'
+    }
+});
